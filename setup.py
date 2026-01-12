0bf1381f2873156ee103289e4508798de1225b82
quamrul hoda <qhoda489@gmail.com>
2026-01-12 13:13:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cellSegmentation",
+    version="0.0.1",
+    author="quamurl-hoda",
+    author_email="qhoda434@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
